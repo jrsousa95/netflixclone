6dfc2c229bea2e644b17140a4a2612abffebba56
Eudes Junior <eudes.sousa@proluno.com.br>
2022-12-20 11:34:15 -0300

    ajustes para reativação

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import Tmdb from './Tmdb';
-import MovieRow from './components/MovieRow';
-import './App.css';
-import FeaturedMovie from './components/FeaturedMovie';
-import Header from './components/Header';
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import "./App.css";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
-export default () => {
-
+export default function App() {
   const [movieList, setMovieList] = useState([]);
   const [featureData, setFeatureData] = useState();
   const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
-      //Pegando a lista total! 
+      //Pegando a lista total!
       let list = await Tmdb.getHomeList();
       setMovieList(list);
 
       //Pegando o Featured
-      let originals = list.filter(i=>i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
       setFeatureData(chosenInfo);
-    }
+    };
 
     loadAll();
   }, []);
@@ -31,30 +32,27 @@ export default () => {
   useEffect(() => {
     const scrollListener = () => {
       if (window.scrollY > 10) {
-        setBlackHeader (true);
+        setBlackHeader(true);
       } else {
-          setBlackHeader (false);
-        }
+        setBlackHeader(false);
       }
+    };
 
-    window.addEventListener('scroll', scrollListener);
+    window.addEventListener("scroll", scrollListener);
 
     return () => {
-      window.removeEventListener('scroll', scrollListener);
-    }
+      window.removeEventListener("scroll", scrollListener);
+    };
   }, []);
 
   return (
     <div className="page">
-
       <Header black={blackHeader} />
 
-      {featureData &&
-      <FeaturedMovie  item={featureData}/> 
-      }
+      {featureData && <FeaturedMovie item={featureData} />}
 
       <section className="lists">
-        {movieList.map((item, key)=>(
+        {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
@@ -64,11 +62,14 @@ export default () => {
         Dados adquiridos do site themoviedb.org<br></br>
       </footer>
 
-      {movieList <=0 &&
+      {movieList <= 0 && (
         <div className="loading">
-          <img src="https://c.tenor.com/Rfyx9OkRI38AAAAM/netflix-netflix-startup.gif" alt="carregando"/>
+          <img
+            src="https://c.tenor.com/Rfyx9OkRI38AAAAM/netflix-netflix-startup.gif"
+            alt="carregando"
+          />
         </div>
-      }
+      )}
     </div>
   );
 }
